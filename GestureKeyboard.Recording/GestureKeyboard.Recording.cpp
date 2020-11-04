bffb3115ebbe28e852a39139826d5e84df86d54b
Jake Gillenwater <JakeGill70@gmail.com>
2020-11-04 14:56:48 -0500

    Console Menu

--- GestureKeyboard.Recording/GestureKeyboard.Recording.cpp
+++ GestureKeyboard.Recording/GestureKeyboard.Recording.cpp
@@ -8,10 +8,12 @@
 #include "LightPenTracker.h"
 #include <vector>
 #include "Bitmap.h"
+#include <conio.h>
 
 using namespace std;
 
 vector<string> getWordList();
+void ClearScreen();
 
 int main()
 {
@@ -21,12 +23,58 @@ int main()
 
     vector<string> wordList = getWordList();
 
-    // I know, I know...
-    int pVal = system("python simpleTypingTest.py");
+    std::string menu = "[1] Setup\n";
+    menu += "[2] Traditional Typing Test(Part 1)\n[3] Traditional Typing Test(Part 2)\n";
+    menu += "[4] WGK Typing Test (Part 1)\n[5] WGK Typing Test (Part 2)\n[6] WGK Typing Test (Part 3)\n[7] WGK Typing Test (Part 4)\n";
+    menu += "[8] Exit\n";
 
-    std::cout << "Python return: " << pVal;
+    std::string input;
+    int inputVal;
+    bool isRunning = true;
 
-    lpt->run(wordList, true);
+    while (isRunning) {
+
+        inputVal = 0;
+
+        ClearScreen();
+        std::cout << menu;
+        std::cin >> input;
+        try {
+            inputVal = stoi(input);
+        }
+        catch (invalid_argument e) {
+            std::cout << "Menu option " << input << " is unavailable.\n";
+        }
+
+        switch (inputVal) {
+            case 1:
+                lpt->run(wordList, true);
+                break;
+            case 2:
+            case 3:
+                // I know, I know... I'm not happy about this either,
+                // but there will be time to refactor it into something
+                // more effecient and more secure after I get the core
+                // functionality working.
+                system("python simpleTypingTest.py");
+                break;
+            case 4:
+            case 5:
+            case 6:
+            case 7:
+                lpt->run(wordList, false);
+                break;
+            case 8:
+                isRunning = false;
+                break;
+            default:
+                std::cout << "Menu option " << input << " is unavailable.\n";
+                break;
+        }
+    }
+    
+
+    
     
     return 0;
 }
@@ -44,4 +92,13 @@ vector<string> getWordList() {
     }
 
     return wordList;
+}
+
+void ClearScreen()
+{
+    // I hate C++ sometimes...
+    // Source: https://www.cplusplus.com/articles/4z18T05o/
+    for (int n = 0; n < 10; n++) {
+        printf("\n\n\n\n\n\n\n\n\n\n");
+    }
 }

2960a2dd3f1d445704bf26e90c10a184e45dc67a
Jake Gillenwater <JakeGill70@gmail.com>
2021-02-03 13:07:22 -0500

    Updated simple typing test to use cmd line args instead of hardcoded paths

--- GestureKeyboard.Recording/simpleTypingTest.py
+++ GestureKeyboard.Recording/simpleTypingTest.py
@@ -3,13 +3,26 @@ import os
 import random
 import string
 import time
+import sys
 
+# Get command line args
+wordListFileName = sys.argv[1]
+resultsFileName = sys.argv[2]
+wordListStartIndex = int(sys.argv[3])
+wordListEndIndex = int(sys.argv[4])
+
+# Pull the word list from the file
 words = []
-with open('wordList.txt') as f:
+with open(wordListFileName) as f:
     words = [line.rstrip() for line in f]
 
-outFile = open("TypingTestResults.txt", "a")
+# Set the words list to only use the required subset
+words = words[wordListStartIndex:wordListEndIndex]
 
+# Create the output file
+outFile = open(resultsFileName, "a")
+
+# Take the typing test
 while(len(words) > 0):
 
     wordToType = words.pop()

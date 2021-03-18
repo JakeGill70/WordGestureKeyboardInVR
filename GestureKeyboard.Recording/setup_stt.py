b88a38538c479c831a47cf847b14312cb6b6525f
Jake Gillenwater <JakeGill70@gmail.com>
2021-03-17 22:23:26 -0400

    Updated stt setup file

---
+++ GestureKeyboard.Recording/setup_stt.py
@@ -0,0 +1,10 @@
+from distutils.core import setup
+import py2exe
+
+setup(name="SimpleTypingTest",
+      console=['simpleTypingTest.py'],
+      version="1.0",
+      author="Jake Gillenwater",
+      author_email="gillenwaterj@etsu.edu",
+      url="http://jakegillenwater.dev"
+      )

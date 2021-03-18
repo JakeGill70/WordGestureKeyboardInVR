ee64c68a4165adae676a40b3e7ab92f164ab0e1a
Jake Gillenwater <JakeGill70@gmail.com>
2021-03-18 00:39:54 -0400

    Setup_up now includes all necessary packages

--- GestureKeyboard.Recording/setup_up.py
+++ GestureKeyboard.Recording/setup_up.py
@@ -1,10 +1,26 @@
 from distutils.core import setup
 import py2exe
 
+from base64 import decodebytes
+import paramiko
+import pysftp
+import os
+import getpass
+import datetime
+
+includes = ["base64", "paramiko", "pysftp", "os", "sys", "getpass", "datetime", "_cffi_backend"]
+dist_dir = "./up/"
+
+options = {"py2exe":{
+    "includes": includes,
+    "dist_dir": dist_dir
+    }}
+
 setup(name="uploader",
       console=['uploader.py'],
       version="1.0",
       author="Jake Gillenwater",
       author_email="gillenwaterj@etsu.edu",
-      url="http://jakegillenwater.dev"
+      url="http://jakegillenwater.dev",
+      options=options
       )
